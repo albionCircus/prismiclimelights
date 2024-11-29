7987fe14c5d6f90e5a0198b506c665437f4d5a17
AlbionCircus <hello@albioncircus.com>
2024-11-29 16:13:52 +0000

    JSXMapSerializer, Bounded, Logo

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,5 +1,17 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/app/components/Bounded";
+import Heading from "@/app/components/Heading";
+import Logo from "@/app/components/Logo";
+
+const components: JSXMapSerializer = {
+  heading1: ({children}) => (
+    <Heading as="h1" size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">{children}</Heading>
+  ),
+  paragraph: ({children}) => (
+    <p className="text-2xl font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
+  ),
+}
 
 /**
  * Props for `Hero`.
@@ -11,12 +23,10 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for hero (variation: {slice.variation}) Slices
-    </section>
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+      <Logo/>
+      <PrismicRichText field={slice.primary.heading} components={components} />
+    </Bounded>
   );
 };
 
