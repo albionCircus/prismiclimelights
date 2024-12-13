dccbda685d568c924d40ee0c1924082854337a53
AlbionCircus <hello@albioncircus.com>
2024-12-13 10:49:44 +0000

    Footer styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default async function Footer() {
     const client = createClient();
     const settings = await client.getSingle("settings");
-    return <footer className="bg-gray-200 py-6 flex flex-row justify-center items-center">
-            <p className="font-body text-slate-600 text-center">
+    return <footer className="bg-stone-700 py-6 flex flex-row justify-center items-center">
+            <p className="font-body text-white text-center">
                 <small>
                     {settings.data.site_title} &copy; {new Date().getFullYear()} 
-                    <span className="hidden sm:inline font-body text-slate-600 px-2">|</span>
+                    <span className="hidden sm:inline font-body text-white px-2">|</span>
                     <br className="block sm:hidden" />
                     <span>
                         <Link href={"https://albioncircus.com/"}>
--- slices/FormSection/index.tsx
+++ slices/FormSection/index.tsx
@@ -1,7 +1,6 @@
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import ContactForm from "@/app/components/ContactForm";
-import Bounded from "@/app/components/Bounded";
 import Image from "next/image";
 
 /**
@@ -14,8 +13,8 @@ export type FormSectionProps = SliceComponentProps<Content.FormSectionSlice>;
  */
 const FormSection = ({ slice }: FormSectionProps): JSX.Element => {
   return (
-    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-      <div className="bg-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] rounded-3xl p-7 md:p-10 margin0auto grid gap-8 md:grid-cols-2 place-items-center lg:max-w-[1400px]">
+    <section className="relative px-4 pt-4 pb-20 md:py-8 md:pt-6 md:pb-20 lg:pt-8 lg:pb-20">
+      <div className="bg-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] rounded-3xl p-7 md:p-10 margin0auto m-[0 auto 40px] grid gap-8 md:grid-cols-2 place-items-center lg:max-w-[1400px]">
         <article className="w-full">
           <h2 className="text-slate-700 mb-4 md:mb-2">{slice.primary.heading}</h2>
           <PrismicRichText field={slice.primary.description} />
@@ -33,7 +32,7 @@ const FormSection = ({ slice }: FormSectionProps): JSX.Element => {
         </div>
       </div>
       <div className="absolute bg-gradient-to-tr from-orange-500 to-amber-200 -z-10 opacity-50 inset-x-0 bottom-0 h-4/5"></div>
-    </Bounded>
+    </section>
   );
 };
 
